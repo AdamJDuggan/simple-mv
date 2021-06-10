421d4aea2f8e9f6156d624d9ad5e64ba2544ccda
Malcolm Elsworth <malcolm@electricputty.co.uk>
2021-06-10 12:30:53 +0100

    Added tip for creating the form

--- views/enquiry-form.php
+++ views/enquiry-form.php
@@ -1,5 +1,6 @@
 <?php require 'partials/header.php'; ?>
 
 <h1>The enquiry form goes here</h1>
+<p>TIP: Your form will need to POST to '/process-enquiry-form'</p>
 
 <?php require 'partials/footer.php'; ?>
